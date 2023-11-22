2b592d2ba28e37f75e57d0087c90b7b60e523d1a
Lucas <santosbr4511@gmail.com>
2023-11-22 14:57:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
